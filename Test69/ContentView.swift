1f7a76cd50339b2c53b1f159d2154e9ac5bd193e
gentlehr <hgentle45@gmail.com>
2021-09-24 23:00:42 -0500

    Initial Commit

---
+++ Test69/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test69
+//
+//  Created by Harrison Gentle on 9/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test69/Test69App.swift
@@ -0,0 +1,17 @@
+//
+//  Test69App.swift
+//  Test69
+//
+//  Created by Harrison Gentle on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Test69App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
